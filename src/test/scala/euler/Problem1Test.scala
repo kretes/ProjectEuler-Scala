8b658d45dbe943cf8ac1b9e097d847e1d1646ef2
Daniel Miladinov <dmiladinov@wingspan.com>
2013-08-20 00:08:25 -0400

    Solve Problem 1 - Advanced Case.

--- src/test/scala/euler/Problem1Test.scala
+++ src/test/scala/euler/Problem1Test.scala
@@ -8,4 +8,8 @@ class Problem1Test extends FlatSpec with ShouldMatchers {
   "A Problem1 Solution" should "be correct for the simple case" in {
     solution(10) should be (23)
   }
+
+  it should "be correct for the advanced case" in {
+    solution(1000) should be (233168)
+  }
 }
