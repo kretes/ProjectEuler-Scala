2fe28d4b39b9fd731f10f61790174f83b976f4de
Daniel Miladinov <dmiladinov@wingspan.com>
2013-10-24 00:52:24 -0400

    Simplify the Problem 8 Solution

--- src/main/scala/euler/Problem8.scala
+++ src/main/scala/euler/Problem8.scala
@@ -1,10 +1,11 @@
 package euler
 
 object Problem8 {
+  def toInt (c: Char): Int = Integer.parseInt(c.toString)
+
+  def multiply (x: Int, y: Int) = x * y
+
   def solution (s: String): Int = {
-    (0 to s.length - 5)
-      .map(i => s.substring(i, i + 5)
-      .map(c => Integer.parseInt(c.toString))
-      .reduce(_ * _)).max
+    (0 to s.length - 5).map(i => s.substring(i, i + 5) map toInt reduce multiply).max
   }
 }
