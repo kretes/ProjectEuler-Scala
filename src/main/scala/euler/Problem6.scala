d91f5188d1b8099c4070c28724d5db164719d1e2
Daniel Miladinov <dmiladinov@wingspan.com>
2013-10-24 00:42:25 -0400

    Remove unnecessary def
    Instead we can reduce with the built-in sum method on Seq rather than
    redefining it here needlessly.

--- src/main/scala/euler/Problem6.scala
+++ src/main/scala/euler/Problem6.scala
@@ -1,17 +1,11 @@
 package euler
 
 object Problem6 {
-  def sum (x: Int, y: Int): Int =
-    x + y
+  def square (x: Int): Int = x * x
 
-  def square (x: Int): Int =
-    x * x
+  def sumOfTheSquaresFromOneTo (x: Int): Int = (1 to x map square).sum
 
-  def sumOfTheSquaresFromOneTo (x: Int): Int =
-    1 to x map square reduce sum
-
-  def squareOfTheSumsFromOneTo (x: Int): Int =
-    square(1 to x reduce sum)
+  def squareOfTheSumsFromOneTo (x: Int): Int = square((1 to x).sum)
 
   def differenceBetweenSquareOfSumsAndSumOfSquaresFromOneTo (x: Int): Int =
     squareOfTheSumsFromOneTo(x) - sumOfTheSquaresFromOneTo(x)
