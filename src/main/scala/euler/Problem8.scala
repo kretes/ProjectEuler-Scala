5deea8a7d91ed13403a82773b01ceb9121937847
Daniel Miladinov <dmiladinov@wingspan.com>
2013-10-24 16:16:09 -0400

    Simplify the Problem 8 Solution even more

--- src/main/scala/euler/Problem8.scala
+++ src/main/scala/euler/Problem8.scala
@@ -3,7 +3,5 @@ package euler
 object Problem8 {
   def toInt (c: Char): Int = Integer.parseInt(c.toString)
 
-  def multiply (x: Int, y: Int) = x * y
-
-  def solution (s: String): Int = ((s sliding 5) map (_ map toInt reduce multiply)).max
+  def solution (s: String): Int = ((s sliding 5) map (_ map toInt reduce (_ * _))).max
 }
