4f13b2a2612070dfc078857868c6f7a655d77acb
Daniel Miladinov <dmiladinov@wingspan.com>
2013-11-03 11:41:08 -0500

    Problem 17 - TextVersion of a Number, Part I
    Works from positive numbers, zero to 99

---
+++ src/main/scala/euler/Problem17.scala
@@ -0,0 +1,9 @@
+package euler
+
+import euler.tools.TextVersion
+
+object Problem17 {
+  def toText (number: Int): String = {
+    TextVersion.of(number)
+  }
+}
---
+++ src/test/scala/euler/Problem17Test.scala
@@ -0,0 +1,154 @@
+package euler
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class Problem17Test extends FlatSpec with ShouldMatchers {
+  "A Problem17 Solution" should "be able to convert 0 to zero" in {
+    Problem17.toText(0) should be ("zero")
+  }
+
+  it should "be able to convert 1 to one" in {
+    Problem17.toText(1) should be ("one")
+  }
+
+  it should "be able to convert 2 to two" in {
+    Problem17.toText(2) should be ("two")
+  }
+
+  it should "be able to convert 3 to three" in {
+    Problem17.toText(3) should be ("three")
+  }
+
+  it should "be able to convert 4 to four" in {
+    Problem17.toText(4) should be ("four")
+  }
+
+  it should "be able to convert 5 to five" in {
+    Problem17.toText(5) should be ("five")
+  }
+
+  it should "be able to convert 6 to six" in {
+    Problem17.toText(6) should be ("six")
+  }
+
+  it should "be able to convert 7 to seven" in {
+    Problem17.toText(7) should be ("seven")
+  }
+
+  it should "be able to convert 8 to eight" in {
+    Problem17.toText(8) should be ("eight")
+  }
+
+  it should "be able to convert 9 to nine" in {
+    Problem17.toText(9) should be ("nine")
+  }
+
+  it should "be able to convert 10 to ten" in {
+    Problem17.toText(10) should be ("ten")
+  }
+
+  it should "be able to convert 11 to eleven" in {
+    Problem17.toText(11) should be ("eleven")
+  }
+
+  it should "be able to convert 12 to twelve" in {
+    Problem17.toText(12) should be ("twelve")
+  }
+
+  it should "be able to convert 13 to thirteen" in {
+    Problem17.toText(13) should be ("thirteen")
+  }
+
+  it should "be able to convert 14 to fourteen" in {
+    Problem17.toText(14) should be ("fourteen")
+  }
+
+  it should "be able to convert 15 to fifteen" in {
+    Problem17.toText(15) should be ("fifteen")
+  }
+
+  it should "be able to convert 16 to sixteen" in {
+    Problem17.toText(16) should be ("sixteen")
+  }
+
+  it should "be able to convert 17 to seventeen" in {
+    Problem17.toText(17) should be ("seventeen")
+  }
+
+  it should "be able to convert 18 to eighteen" in {
+    Problem17.toText(18) should be ("eighteen")
+  }
+
+  it should "be able to convert 19 to nineteen" in {
+    Problem17.toText(19) should be ("nineteen")
+  }
+
+  it should "be able to convert 20 to twenty" in {
+    Problem17.toText(20) should be ("twenty")
+  }
+
+  it should "be able to convert 21 to twenty-one" in {
+    Problem17.toText(21) should be ("twenty-one")
+  }
+
+  it should "be able to convert 27 to twenty-seven" in {
+    Problem17.toText(27) should be ("twenty-seven")
+  }
+
+  it should "be able to convert 30 to thirty" in {
+    Problem17.toText(30) should be ("thirty")
+  }
+
+  it should "be able to convert 38 to thirty-eight" in {
+    Problem17.toText(38) should be ("thirty-eight")
+  }
+
+  it should "be able to convert 40 to forty" in {
+    Problem17.toText(40) should be ("forty")
+  }
+
+  it should "be able to convert 42 to forty-two" in {
+    Problem17.toText(42) should be ("forty-two")
+  }
+
+  it should "be able to convert 50 to fifty" in {
+    Problem17.toText(50) should be ("fifty")
+  }
+
+  it should "be able to convert 53 to fifty-three" in {
+    Problem17.toText(53) should be ("fifty-three")
+  }
+
+  it should "be able to convert 60 to sixty" in {
+    Problem17.toText(60) should be ("sixty")
+  }
+
+  it should "be able to convert 64 to sixty-four" in {
+    Problem17.toText(64) should be ("sixty-four")
+  }
+
+  it should "be able to convert 70 to seventy" in {
+    Problem17.toText(70) should be ("seventy")
+  }
+
+  it should "be able to convert 75 to seventy-five" in {
+    Problem17.toText(75) should be ("seventy-five")
+  }
+
+  it should "be able to convert 80 to eighty" in {
+    Problem17.toText(80) should be ("eighty")
+  }
+
+  it should "be able to convert 86 to eighty-six" in {
+    Problem17.toText(86) should be ("eighty-six")
+  }
+
+  it should "be able to convert 90 to ninety" in {
+    Problem17.toText(90) should be ("ninety")
+  }
+
+  it should "be able to convert 99 to seventy-five" in {
+    Problem17.toText(99) should be ("ninety-nine")
+  }
+}
