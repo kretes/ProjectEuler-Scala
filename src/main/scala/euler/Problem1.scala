3f89c1688100df2212202b4ab456504243c38e2f
Daniel Miladinov <dmiladinov@wingspan.com>
2013-11-13 23:03:55 -0500

    Use a Range instead of Array.range for Problem1

--- src/main/scala/euler/Problem1.scala
+++ src/main/scala/euler/Problem1.scala
@@ -2,6 +2,6 @@ package euler
 
 object Problem1 {
   def solution(limit: Int): Int = {
-    Array.range(1, limit).filter(x => x % 3 == 0 || x % 5 == 0).sum
+    (1 to limit).filter(x => x % 3 == 0 || x % 5 == 0).sum
   }
 }
