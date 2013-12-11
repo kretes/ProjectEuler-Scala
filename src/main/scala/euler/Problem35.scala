d50298de43054f527c1686e44015998fbe37542c
Daniel Miladinov <dmiladinov@wingspan.com>
2013-12-11 14:08:43 -0500

    Restate Problem 35's Solution with fewer implicits

--- src/main/scala/euler/Problem35.scala
+++ src/main/scala/euler/Problem35.scala
@@ -1,8 +1,29 @@
 package euler
 import euler.tools.ImplicitConversions._
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 
 object Problem35 {
   def solution (upperBound: Long): Seq[Long] = {
-    (2L until upperBound).filter(_.isCircularPrime)
+    (2L until upperBound).filter(isCircularPrime)
+  }
+
+  private def isCircularPrime (l: Long): Boolean = {
+    rotations(l.toString).forall(_.toLong.isPrime)
+  }
+
+  private def rotations(str: String): List[String] = {
+    @tailrec
+    def makeRotations (s: String, buff: ListBuffer[String] = ListBuffer(str)): List[String] = {
+      val rotated = s.tail + s.head
+
+      if (rotated != str) {
+        makeRotations(rotated, buff += rotated)
+      } else {
+        buff.toList
+      }
+    }
+
+    makeRotations(str)
   }
 }
