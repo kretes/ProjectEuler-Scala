a50f1228c3030aa0c1fa7ac0abcf99d22843944c
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-19 11:11:39 +0200

    problem 1 solution

--- src/main/scala/euler/Problem1.scala
+++ src/main/scala/euler/Problem1.scala
@@ -1,5 +1,7 @@
 package euler
 
 object Problem1 {
-  def solution(limit: Int): Int = ???
+  def solution(limit: Int): Int = {
+    (1 to limit-1).filter(number => number % 3 ==0 || number % 5 ==0).sum
+  }
 }
