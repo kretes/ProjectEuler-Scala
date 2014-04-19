45594d5a38163ea642b0c5345c4d8673c0113f27
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-19 11:17:11 +0200

    problem 2 solution

--- src/main/scala/euler/Problem2.scala
+++ src/main/scala/euler/Problem2.scala
@@ -2,5 +2,9 @@ package euler
 
 object Problem2 {
 
-  def solution (highestFibonacci: Int): Int = ???
+  val fibonacciStream: Stream[Int] = 1 #:: fibonacciStream.scanLeft(1)(_ + _)
+
+  def fibonacciUpTo(highest: Int) : Stream[Int]= fibonacciStream.takeWhile(_ < highest)
+
+  def solution (highestFibonacci: Int): Int = fibonacciUpTo(highestFibonacci).filter(_ % 2 == 0).sum
 }
