c71c2e22dfeb57fd636bec8429921fdb7db46f47
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-21 12:48:59 +0200

    problem 4 brute force solution

--- src/main/scala/euler/Problem4.scala
+++ src/main/scala/euler/Problem4.scala
@@ -1,7 +1,11 @@
 package euler
 
 object Problem4 {
-  def isPalindrome (x: String): Boolean = ???
+  def palindromeProductOf(digits: Int):Int = {
+    (for (a <- (2 to Math.pow(10,digits).toInt); b <- (2 to Math.pow(10,digits).toInt); if isPalindrome((a * b).toString)) yield a * b).max
+  }
 
-  def solution: Int = ???
+  def isPalindrome (x: String): Boolean = x == x.reverse
+
+  def solution: Int = palindromeProductOf(3);
 }
--- src/test/scala/euler/Problem4Test.scala
+++ src/test/scala/euler/Problem4Test.scala
@@ -5,6 +5,10 @@ import org.scalatest.matchers.ShouldMatchers
 import euler.Problem4.solution
 
 class Problem4Test extends FlatSpec with ShouldMatchers {
+  "A Problem4 Solution" should "calculate simple answer" in {
+    Problem4.palindromeProductOf(2) should be (9009)
+  }
+
   "A Problem4 Solution" should "return the correct answer" in {
     solution should be (906609)
   }
