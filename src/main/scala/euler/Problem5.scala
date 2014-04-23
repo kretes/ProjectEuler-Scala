a76326a9b600979bbaf46527d536196fcec9780c
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-23 20:51:25 +0200

    problem 5 refactored

--- src/main/scala/euler/Problem5.scala
+++ src/main/scala/euler/Problem5.scala
@@ -14,27 +14,17 @@ object Problem5 {
   }
 
   def solution (x: Long) : Long = {
-    val listOfGrouppedPrimeFactors: IndexedSeq[Map[Factor, Count]] = (2 to x.toInt).map(primeFactorsGroupped(_))
-    val uniquePrimeFactors: Map[Factor, Count] = mergeToMap(listOfGrouppedPrimeFactors)
+    val listOfGrouppedPrimeFactors: Seq[Map[Factor, Count]] = (2 to x.toInt).map(primeFactorsGroupped(_))
+    val uniquePrimeFactors: Map[Factor, Count] = listOfGrouppedPrimeFactors.reduce(merge)
     uniquePrimeFactors.map(entry => Math.pow(entry._1.factor,entry._2.count)).product.toLong ;
   }
 
-
-  def mergeToMap(listOfGrouppedPrimeFactors: IndexedSeq[Map[Problem5.Factor, Problem5.Count]]): Map[Problem5.Factor, Problem5.Count] = {
-    listOfGrouppedPrimeFactors.reduce(merge)
-
-  }
+  def max(option1: Option[Long], option2: Option[Long]) = Math.max(option1.getOrElse(0L), option2.getOrElse(0L))
 
   def merge(histogram1: Map[Factor, Count], histogram2: Map[Factor, Count]): Map[Factor, Count] = {
     val keys = histogram1.keySet ++ histogram2.keySet
-    val set: Set[(Factor, Count)] = for(key <- keys) yield (key,new Count(Math.max(histogram1.get(key).getOrElse(new Count(0)).count, histogram2.get(key).getOrElse(new Count(0)).count)))
+    val set: Set[(Factor, Count)] = for(key <- keys) yield (key,new Count(max(histogram1.get(key).map(_.count), histogram2.get(key).map(_.count))))
     set.toMap
-//
-//
-//    val histograms: List[(Factor,Count)] = histogram1.toList ++ histogram2.toList
-//    val groupByFactor: Map[Factor, List[(Factor, Count)]] = histograms.groupBy(_._1)
-//    val values: Map[Factor, List[Count]] = groupByFactor.mapValues(list => list.map(_._2))
-//    val grouppedByFactor: Map[Factor, Count] = values.mapValues(list => new Count(list.map(_.count).max))
-//    grouppedByFactor
   }
+
 }
