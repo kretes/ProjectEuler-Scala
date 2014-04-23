9886e3fcb2da5122c3ed4e05fefa31ce28563ea4
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-23 21:31:28 +0200

    problem 9 optimized solution

--- src/main/scala/euler/Problem9.scala
+++ src/main/scala/euler/Problem9.scala
@@ -5,8 +5,7 @@ object Problem9 {
   def solution (sumConstraint: Int): Option[(Int, Int, Int)] = {
     (for (a <- 1 to sumConstraint-2;
          b <- 1 to sumConstraint-2;
-         c <- 1 to sumConstraint-2;
-         if(a+b+c== sumConstraint);
+         c = sumConstraint - (a+b);
         if(a*a + b*b == c*c)) yield (a,b,c)).headOption
   }
 }
