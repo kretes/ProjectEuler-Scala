25da2c139c07fc970e7b940486063b2bfb7c5567
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-23 21:12:00 +0200

    problem 9 naive solution

--- src/main/scala/euler/Problem9.scala
+++ src/main/scala/euler/Problem9.scala
@@ -2,6 +2,12 @@ package euler
 
 object Problem9 {
 
-  def solution (sumConstraint: Int): Option[(Int, Int, Int)] = ???
+  def solution (sumConstraint: Int): Option[(Int, Int, Int)] = {
+    (for (a <- 1 to sumConstraint-2;
+         b <- 1 to sumConstraint-2;
+         c <- 1 to sumConstraint-2;
+         if(a+b+c== sumConstraint);
+        if(a*a + b*b == c*c)) yield (a,b,c)).headOption
+  }
 }
 
