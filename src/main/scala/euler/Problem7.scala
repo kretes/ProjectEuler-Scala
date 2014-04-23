da66344501d30881fc3e23a1147e2a22247d87ae
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-23 20:55:14 +0200

    problem 7 naive solution

--- src/main/scala/euler/Problem7.scala
+++ src/main/scala/euler/Problem7.scala
@@ -2,7 +2,7 @@ package euler
 
 
 object Problem7 {
-  def primesFrom (start: Long): Stream[Long] = ???
+  def primesFrom (start: Long): Stream[Long] = if(Problem3.primeFactorOf(start) == start) start #:: primesFrom(start+1) else primesFrom(start+1)
 
-  def nthPrime (n: Int): Long = ???
+  def nthPrime (n: Int): Long = primesFrom(2)(n)
 }
