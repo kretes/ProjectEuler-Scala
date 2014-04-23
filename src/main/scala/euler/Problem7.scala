1895e5b4ab242267a9f43722a2311fc2295968f1
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-23 21:06:06 +0200

    problem 7 prime search with looking into only currently calculated primes

--- src/main/scala/euler/Problem7.scala
+++ src/main/scala/euler/Problem7.scala
@@ -1,8 +1,15 @@
 package euler
 
+import scala.annotation.tailrec
+
 
 object Problem7 {
-  def primesFrom (start: Long): Stream[Long] = if(Problem3.primeFactorOf(start) == start) start #:: primesFrom(start+1) else primesFrom(start+1)
 
-  def nthPrime (n: Int): Long = primesFrom(2)(n)
+  def isPrime(n: Long)= if(n ==2) true else primes.takeWhile(_ < n / 2).find(n % _ == 0).map(_ == n).getOrElse(true)
+
+  def primesFrom (start: Long): Stream[Long] = if(isPrime(start)) start #:: primesFrom(start+1) else primesFrom(start+1)
+
+  val primes: Stream[Long] = 2 #:: primesFrom(3)
+
+  def nthPrime (n: Int): Long = primes(n)
 }
