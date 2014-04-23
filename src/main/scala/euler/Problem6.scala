57849541b973b143f9381ea2f11de66af6b91771
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-23 20:51:37 +0200

    problem 6 naive solution

--- src/main/scala/euler/Problem6.scala
+++ src/main/scala/euler/Problem6.scala
@@ -1,12 +1,12 @@
 package euler
 
 object Problem6 {
-  def square (x: Int): Int = ???
+  def square (x: Int): Int = x * x
 
-  def sumOfTheSquaresFromOneTo (x: Int): Int = ???
+  def sumOfTheSquaresFromOneTo (x: Int): Int = (1 to x).foldLeft(0)((acc, num) => acc + square(num))
 
-  def squareOfTheSumsFromOneTo (x: Int): Int = ???
+  def squareOfTheSumsFromOneTo (x: Int): Int = square((1 to x).sum)
 
-  def differenceBetweenSquareOfSumsAndSumOfSquaresFromOneTo (x: Int): Int = ???
+  def differenceBetweenSquareOfSumsAndSumOfSquaresFromOneTo (x: Int): Int = squareOfTheSumsFromOneTo(x) - sumOfTheSquaresFromOneTo(x)
 }
 
