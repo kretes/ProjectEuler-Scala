3c2e84421ac7a4065e7862912de1d3d6a7de60e4
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-26 00:21:52 +0200

    problem 10 optimized solution finished in 10 minutes.. :/

--- src/main/scala/euler/Problem10.scala
+++ src/main/scala/euler/Problem10.scala
@@ -6,20 +6,26 @@ import scala.collection.GenSeq
 
 object Problem10 {
 
-  //350 sec
+  //350 sec - prime fac
+  //644 naive erastothen....
+  //???
 
   def extendUpTo(value: Long, primeMultipliers: GenSeq[(Long, Long)]): GenSeq[(Long, Long)] =
-    primeMultipliers.map((tuple:(Long,Long)) => if (tuple._2 >= value) tuple else (tuple._1, value + tuple._1 - (if (value % tuple._1 == 0) tuple._1 else value % tuple._1)))
+    primeMultipliers.map((tuple:(Long,Long)) => if (tuple._2 >= value) tuple else
+      (tuple._1, value + (tuple._1 - (value % tuple._1)) % tuple._1))
 
-  def calculatePrimes(start: Long, primeMultipliers: GenSeq[(Long,Long)]) : Stream[Long] = {
-    if ( primeMultipliers.find(_._2 == start).isDefined) calculatePrimes(start+1,primeMultipliers)
-    else if ( primeMultipliers.find(_._2 < start).isDefined) calculatePrimes(start,extendUpTo(start,primeMultipliers))
-    else start #:: calculatePrimes(start+1,primeMultipliers :+ (start,start))
+
+  def calculatePrimes(max:Long,start:Long, primeMultipliers: GenSeq[(Long,Long)]) : GenSeq[(Long,Long)] = {
+    if( start % 10000 == 0) println(s"for $start we have ${primeMultipliers.size} at ${System.currentTimeMillis()}")
+    if (start == max) primeMultipliers
+    else if (primeMultipliers.find(_._2 == start).isDefined) calculatePrimes(max, start + 1, primeMultipliers)
+    else if (primeMultipliers.find(_._2 < start).isDefined) calculatePrimes(max, start, extendUpTo(start, primeMultipliers))
+    else calculatePrimes(max, start + 1, primeMultipliers :+(start, start))
   }
 
-  val primes = 2 #:: calculatePrimes(3, Vector((2L,2L)))
 
-  def solution (limit: Long): Long = primes.takeWhile(_ < limit).sum
+
+  def solution (limit: Long): Long = calculatePrimes(limit.toLong,3, Vector((2L,2L))).map(_._1).sum
 
 
 }
