93f01733398f2b1b3d897324558a9363dbcacc72
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-25 22:28:17 +0200

    problem 10 optimized solution but does not fit within memory limits

--- src/main/scala/euler/Problem10.scala
+++ src/main/scala/euler/Problem10.scala
@@ -1,23 +1,23 @@
 package euler
 
 import scala.annotation.tailrec
+import scala.collection.GenSeq
 
 
 object Problem10 {
 
-  implicit def int2Long(x: Int): Long = x.toLong
   //350 sec
 
-  def extendUpTo(value: Long, primeMultipliers: Seq[(Long, Long)]): Seq[(Long, Long)] =
-    primeMultipliers.map((tuple:(Long,Long)) => if (tuple._2 >= value) tuple else (tuple._1, value + tuple._1 - (value % tuple._1)))
+  def extendUpTo(value: Long, primeMultipliers: GenSeq[(Long, Long)]): GenSeq[(Long, Long)] =
+    primeMultipliers.map((tuple:(Long,Long)) => if (tuple._2 >= value) tuple else (tuple._1, value + tuple._1 - (if (value % tuple._1 == 0) tuple._1 else value % tuple._1)))
 
-  def calculatePrimes(start: Long, primeMultipliers: Seq[(Long,Long)]) : Stream[Long] = {
-    if ( primeMultipliers.find(_._2 < start).isDefined) calculatePrimes(start,extendUpTo(start,primeMultipliers))
-    else if ( primeMultipliers.find(_._2 == start).isDefined) calculatePrimes(start+1,primeMultipliers)
+  def calculatePrimes(start: Long, primeMultipliers: GenSeq[(Long,Long)]) : Stream[Long] = {
+    if ( primeMultipliers.find(_._2 == start).isDefined) calculatePrimes(start+1,primeMultipliers)
+    else if ( primeMultipliers.find(_._2 < start).isDefined) calculatePrimes(start,extendUpTo(start,primeMultipliers))
     else start #:: calculatePrimes(start+1,primeMultipliers :+ (start,start))
   }
 
-  val primes = 2 #:: calculatePrimes(3, Vector((2,2)))
+  val primes = 2 #:: calculatePrimes(3, Vector((2L,2L)))
 
   def solution (limit: Long): Long = primes.takeWhile(_ < limit).sum
 
--- src/test/scala/euler/Problem10Test.scala
+++ src/test/scala/euler/Problem10Test.scala
@@ -4,34 +4,49 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
 class Problem10Test extends FlatSpec with ShouldMatchers {
-  "A Problem10 Solution" should "return the correct answer for the sum of primes below 10" in {
+  "A Problem10 Solution" should "work for sum of primes below 10" in {
     Problem10.solution(10) should be (17)
   }
 
-  "A Problem10 Solution" should "return the correct answer for the sum of primes below 1000" in {
+//  it should "calculate primes below 20" in {
+//    Problem10.primes.takeWhile(_ < 20).toList should be (List(2, 3,  5,  7,  11,  13,  17,  19))
+//  }
+
+  it should "work for sum of primes below 20" in {
+    Problem10.solution(20) should be (77)
+  }
+
+  it should "work for sum of primes below 1000" in {
     Problem10.solution(1000) should be (76127)
   }
 
-  //0.8 -> 0.3 (find1) -> 0.2(find2)
-  it should "return the correct answer for the sum of primes below 10000" in {
+  //0.8 -> 0.3 (find1) -> 0.2(find2) -> 0.1 (order)
+  it should "work for sum of primes below 10000" in {
     Problem10.solution(10000L) should be (5736396L)
   }
 
-  //18 -> 9(find1) -> 7(find2) -> 1.8(Vec)
-  it should "return the correct answer for the sum of primes below 50000" in {
+  //18 -> 9(find1) -> 7(find2) -> 1.8(Vec) -> 0.5 (order)
+  it should "work for sum of primes below 50000" in {
     Problem10.solution(50000L) should be (121013308L)
   }
 
-  //27 (find1) -> 18(find2) -> 5(Vec)
-  it should "return the correct answer for the sum of primes below 100000" in {
+  //27 (find1) -> 18(find2) -> 5(Vec) -> 1.7(order)
+  it should "work for sum of primes below 100000" in {
     Problem10.solution(100000L) should be (454396537L)
   }
 
-  it should "return the correct answer for the sum of primes below 500000" in {
-    Problem10.solution(400000L) should be (17)
+  //?
+  it should "work for sum of primes below 200000" in {
+    Problem10.solution(200000L) should be (454396537L)
   }
 
-  it should "return the correct answer for the sum of primes below 2 million" in {
+  //107(Vec) -> 41 (order)
+  it should "work for sum of primes below 500000" in {
+    Problem10.solution(400000L) should be (6458901531L)
+  }
+
+  //350 (with prime factorization)
+  it should "work for sum of primes below 2 million" in {
     Problem10.solution(2000000L) should be (142913828922L)
   }
 }
