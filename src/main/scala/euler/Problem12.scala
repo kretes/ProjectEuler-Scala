ba3b4940bf7b0a79aaf1a7ed8e495a28ff1c1594
Tomasz Bartczak <kretes@gazeta.pl>
2014-04-27 08:35:29 +0200

    problem 12 slow but elegant solution

--- src/main/scala/euler/Problem12.scala
+++ src/main/scala/euler/Problem12.scala
@@ -1,7 +1,22 @@
 package euler
 
+import scala.annotation.tailrec
+
 
 object Problem12 {
-  def solution (n: Int, memo: Int = 1): Int = ???
+
+  def getTriangleNumbers(previous: Int, toAdd: Int):Stream[Int] = (previous + toAdd) #:: getTriangleNumbers(previous + toAdd, toAdd + 1)
+
+  val triangleNumbers:Stream[Int] = 1 #:: getTriangleNumbers(1,2)
+
+  val naturalNumbers:Stream[Int] = 1 #:: naturalNumbers.scanLeft(2)((a,b) => a + 1)
+
+  def divisors(number: Int) = (1 to number/2).par.filter(number % _ == 0).size
+
+  def countDivisors(number: Int): Int = divisors(number)
+
+  def solution (divisorsCount: Int): Int = {
+    triangleNumbers.find(triangle => {countDivisors(triangle) >= divisorsCount}).getOrElse(0)
+  }
 
 }
--- src/test/scala/euler/Problem12Test.scala
+++ src/test/scala/euler/Problem12Test.scala
@@ -4,6 +4,26 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
 class Problem12Test extends FlatSpec with ShouldMatchers {
+
+  it should "have naturalswork for small problem" in {
+    Problem12.naturalNumbers.take(10).toList should be ((1 to 10).toList)
+  }
+
+  it should "work for small problem" in {
+    Problem12.solution(5) should be (28)
+  }
+
+  //4.7 -> 3.1 par
+  it should "work for medium problem" in {
+    Problem12.solution(200) should be (2031120)
+  }
+
+  //50(par)
+  it should "work for medium 2 problem" in {
+    Problem12.solution(350) should be (17907120)
+  }
+
+  //416 sec
   "A Problem12 Solution" should "return the value of the first triangle number to have over five hundred divisors" in {
     Problem12.solution(500) should be (76576500)
   }
